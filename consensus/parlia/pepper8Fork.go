0c56fa4e91ef81fc6d419ce3b376cf4bdb06508f
Cyril <cyril@chiliz.com>
2025-07-18 12:02:36 +0200

    change DDP bytecode

--- consensus/parlia/pepper8Fork.go
+++ consensus/parlia/pepper8Fork.go
@@ -11,7 +11,7 @@ import (
 const (
 	pepper8MintAmount                    = "148600000000000000000000000"
 	pepper8RecipientAddress              = "0xE0d17A41C1A4Fe527e375C644F9D2A02e96111ED"
-	deterministicDeploymentProxyBytecode = "604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3"
+	deterministicDeploymentProxyBytecode = "7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3"
 	deterministicDeploymentProxyAddress  = "0x4e59b44847b379578588920cA78FbF26c0B4956C"
 )
 
